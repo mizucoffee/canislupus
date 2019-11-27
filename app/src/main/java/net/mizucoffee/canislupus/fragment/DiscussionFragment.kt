77fb229336d6a34455dad14893e08dde30bb1c51
Ritsuki Goto <develop@mizucoffee.net>
2019-11-27 14:54:26 +0900

    [add] card count

--- app/src/main/java/net/mizucoffee/canislupus/fragment/DiscussionFragment.kt
+++ app/src/main/java/net/mizucoffee/canislupus/fragment/DiscussionFragment.kt
@@ -1,17 +1,20 @@
 package net.mizucoffee.canislupus.fragment
 
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_discussion.*
 
 import net.mizucoffee.canislupus.R
 import net.mizucoffee.canislupus.activity.GameActivity
 import net.mizucoffee.canislupus.databinding.FragmentDiscussionBinding
+import net.mizucoffee.canislupus.enumerate.CardEnum
 import net.mizucoffee.canislupus.viewmodel.DiscussionViewModel
 
 class DiscussionFragment : Fragment() {
@@ -34,9 +37,27 @@ class DiscussionFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         activity?.title = "canislupus - 話し合い"
+        var a = 1
+        CardEnum.values().forEach { card ->
+            val count = getGVM().getCardList().count { it.card == card }
+            if (count != 0) {
+                val tv = TextView(activity).apply {
+                    setText("${card.init().name}: ${count}")
+                    textSize = 24f
+                    setTextColor(Color.parseColor("#AAAAAA"))
+                    setPadding(dp2px(8),dp2px(2),dp2px(8),dp2px(2))
+                }
+                if (a % 2 != 0) cardList1.addView(tv) else cardList2.addView(tv)
+                a++
+                println(card.name)
+            }
+        }
+
         binding.viewModel?.startTimer()
         binding.viewModel?.also { observeTransition(it) }
     }
+
+    private fun dp2px(dp: Int) = (dp * resources.displayMetrics.density + 0.5f).toInt()
 
     private fun observeTransition(viewModel: DiscussionViewModel) {
         viewModel.transition.observe(this, Observer {
