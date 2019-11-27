fd60bb2b72a7ebeb4a7ac3cde2c54332afedf427
Ritsuki Goto <develop@mizucoffee.net>
2019-11-27 11:12:12 +0900

    [add] license

--- app/src/main/java/net/mizucoffee/canislupus/activity/MainActivity.kt
+++ app/src/main/java/net/mizucoffee/canislupus/activity/MainActivity.kt
@@ -3,8 +3,13 @@ package net.mizucoffee.canislupus.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import net.mizucoffee.canislupus.R
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +19,19 @@ class MainActivity : AppCompatActivity() {
         gameStartBtn.setOnClickListener {
             startActivity(Intent(applicationContext, GameActivity::class.java))
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_license) {
+            val intent = Intent(this, OssLicensesMenuActivity::class.java)
+            intent.putExtra("title", "Open Source License")
+            startActivity(intent)
+        }
+        return true
     }
 }
